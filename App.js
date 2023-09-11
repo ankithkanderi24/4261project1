e0a77f47a8089c01c73b577b0c9ba3aba9353413
ankithkanderi24 <ankithkanderi@outlook.com>
2023-09-11 10:21:42 -0400

    init commit

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'; 
+import { View, TextInput, Text, Button, StyleSheet, TouchableOpacity} from 'react-native';
 
 export default function App() {
+  const [playername, setName] = useState('');
+  const [ppg, setPpg] = useState('');
+  const [dataSubmitted, setDataSubmitted] = useState(false);
+
+  const handleSubmit = () => {
+    setDataSubmitted(true);
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style = {styles.text}>Type your name below!</Text>
+      <TextInput style = {styles.textinput}
+        value={playername}
+        onChangeText={text => setName(text)}
+        placeholder="Name"
+      />
+      <Text style = {styles.text}>Type how many points you scored below!</Text>
+      <TextInput style = {styles.textinput}
+        value={ppg}
+        onChangeText={text => setPpg(text)}
+        placeholder="PPG"
+      />
+      <Button title="Submit Data" onPress = {handleSubmit}/>
+      {dataSubmitted && (
+        <View>
+          <Text style={styles.resultText}>Name: {playername}</Text>
+          <Text style={styles.resultText}>PPG: {ppg}</Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -17,4 +42,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'blue',
+  },
+  textinput: {
+    fontSize: 20,
+  },
+  resultText: {
+    fontSize: 20
+  }
 });
+
+
+
+
---
+++ components/config.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFireStore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBlyiF83MT5K4X34lJMO6f1gmC2WmdPY3k",
+  authDomain: "project1-90cf3.firebaseapp.com",
+  projectId: "project1-90cf3",
+  storageBucket: "project1-90cf3.appspot.com",
+  messagingSenderId: "625838080182",
+  appId: "1:625838080182:web:9151733b46a95589e1e617",
+  measurementId: "G-4VT55LQ821"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Firestore
+export const db = getFireStore(app);
